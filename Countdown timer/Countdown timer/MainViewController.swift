145805208e919b7ddec0b77732a930b915762bb9
Эван Крошкин <>
2022-07-02 21:56:41 +0300

    Initial project setup

---
+++ Countdown timer/Countdown timer/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countdown timer
+//
+//  Created by Эван Крошкин on 2.07.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
---
+++ Countdown timer/Countdown timer/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  Countdown timer
+//
+//  Created by Эван Крошкин on 2.07.22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = UINavigationController(rootViewController: MainViewController())
+        window?.makeKeyAndVisible()
+    }
+}
