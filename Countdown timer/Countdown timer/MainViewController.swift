dcb8e05d357ae0cafbb2c75321a615afbd4edb2c
Эван Крошкин <>
2022-07-02 22:07:55 +0300

    ADDED exstension UIView

--- Countdown timer/Countdown timer/MainViewController.swift
+++ Countdown timer/Countdown timer/MainViewController.swift
@@ -11,7 +11,6 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
     }
 
 
---
+++ Countdown timer/Countdown timer/TimerStackView.swift
@@ -0,0 +1,14 @@
+//
+//  TimerStackView.swift
+//  Countdown timer
+//
+//  Created by Эван Крошкин on 2.07.22.
+//
+
+import UIKit
+
+class TimerStackView: UIStackView {
+    
+}
+
+
